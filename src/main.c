0850f719e0f14d3db8e1f4d4130fbf129eac9300
vegedra <vegedracp@gmail.com>
2024-08-07 21:02:33 -0300

    exportado de forma automatica em 07-08-2024 21:02

--- src/main.c
+++ src/main.c
@@ -4,18 +4,34 @@
 #include <curses.h>
 #include <stdlib.h>
 
+// Structs
+typedef struct Room {
+	int posX;
+	int posY;
+	int height;
+	int width;
+	//Monster ** monsters;
+	//Item ** items;
+} Room;
+
 typedef struct Player {
 	int posX;
 	int posY;
 	int hp;
 } Player;
 
+// Prototype functions
+
 int screenSetup();
-int mapSetup();
 int handleInput(int input, Player * user);
 int playerMove(int y, int x, Player * user);
 int checkPosition(int newY, int newX, Player * user);
+int drawRoom(Room *room);
+Room ** mapSetup();
 Player * playerSetup();
+
+// Room functions
+Room * createRoom(int x, int y, int height, int width);
 
 int main() {
 	Player * user;
@@ -39,26 +55,60 @@ int screenSetup() {
 	noecho();
 
 	printw("Fantasy Lied by Pedro Ivo, 2024\n");
-	printw("v0.0.3");
+	printw("v0.0.4");
 	refresh();
 	
 	return 0;
 }
 
-int mapSetup() {
-	mvprintw(13, 13, "--------");
-	mvprintw(14, 13, "|......|");
-	mvprintw(15, 13, "|......|");
-	mvprintw(16, 13, "|......|");
-	mvprintw(17, 13, "|......|");
-	mvprintw(18, 13, "--------");
+Room ** mapSetup() {
+	Room ** rooms;
+	rooms = malloc(sizeof(Room)*6);
 	
-	mvprintw(2, 40, "----------");
-	mvprintw(3, 40, "|........|");
-	mvprintw(4, 40, "|........|");
-	mvprintw(5, 40, "|........|");
-	mvprintw(6, 40, "|........|");
-	mvprintw(7, 40, "----------");
+	// x, y, height, width
+	rooms[0] = createRoom(13, 13, 6, 8);
+	drawRoom(rooms[0]);
+	
+	rooms[1] = createRoom(40, 10, 6, 11);
+	drawRoom(rooms[1]);
+	
+	rooms[2] = createRoom(60, 3, 9, 8);
+	drawRoom(rooms[2]);
+}
+
+Room * createRoom(int x, int y, int height, int width) {
+	Room * newRoom;
+	newRoom = malloc(sizeof(Room));
+	
+	newRoom->posX = x;
+	newRoom->posY = y;
+	newRoom->height = height;
+	newRoom->width = width;
+	
+	return newRoom;
+}
+
+int drawRoom(Room *room) {
+	int x;
+	int y;
+	
+	// Draws top and bottom
+	for (x = room->posX; x < room->posX + room->width; x++) {
+		mvprintw(room->posY, x, "-"); // Top
+		mvprintw(room->posY + room->height - 1, x, "-"); // Bottom
+	}
+	// Draws side-walls
+	for (y = room->posY + 1; y < room->posY + room->height - 1; y++) {
+		mvprintw(y, room->posX, "|"); 
+		mvprintw(y, room->posX + room->width - 1, "|");
+		
+		// Draws the floors
+		for (x = room->posX + 1; x < room->posX + room->width - 1; x++) {
+			mvprintw(y, x, "."); 
+		}
+	}
+	
+	return 0;
 }
 
 Player * playerSetup() {
